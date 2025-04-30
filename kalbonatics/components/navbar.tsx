b79854a18cf1f2305e25a2aeef333706e42b89b1
ranniellbanzonn <ranniellbanzon@gmail.com>
2025-04-30 16:14:14 +0800

    Change heads and tails  image

--- kalbonatics/components/navbar.tsx
+++ kalbonatics/components/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between text-[#191919] items-center px-[32px] w-full max-w-[1440px] py-[32px] px-[10px]">
+    <nav className="flex justify-between text-[#191919] items-center px-[32px] w-full max-w-[1440px] py-[32px] ">
       <div className="p-4 bg-[#E5E5CB] rounded-[8px]">
         <p className="font-extrabold">KALFLIP</p>
       </div>

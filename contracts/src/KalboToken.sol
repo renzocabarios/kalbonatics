2781003682586efa8bf19c8933024420738bbb8d
bordinano <vordigallardo@gmail.com>
2025-05-05 18:47:42 +0800

    Add ERC20 Token

--- contracts/src/KalboToken.sol
+++ contracts/src/KalboToken.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract KalboToken {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract KalboToken is ERC20 {
+    
+    constructor () ERC20 ("Kalbo Token", "KALBO"){
+        _mint(msg.sender, 1_000_000_000_000 * 10 ** decimals());
     }
 
-    function increment() public {
-        number++;
-    }
 }

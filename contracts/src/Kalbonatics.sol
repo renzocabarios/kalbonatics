5c48b4b442edb93c669ccc213cdcb3cc9b28b417
renzocabarios <renzo.cabarios@gmail.com>
2025-05-05 17:15:44 +0800

    Update contracts

--- contracts/src/Kalbonatics.sol
+++ contracts/src/Kalbonatics.sol
@@ -1,4 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "./Registry.sol";
 
-contract Kalbonatics {}
+// TODO: Add 3% of kalbotokens as terminal fee to bet
+contract Kalbonatics {
+    event UserBetEvent(uint amount, string guess, address user);
+    event SetRewardsEvent(uint amount, address user);
+
+    address public owner;
+
+    mapping(address => uint) userRewards;
+
+    Registry registry;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function bet(uint amount, string memory guess) public {
+        require(isValid(guess), "Input must be 'HEADS' or 'TAILS'");
+        emit UserBetEvent(amount, guess, msg.sender);
+    }
+
+    function setRewards(uint amount, address user) public onlyOwner {
+        userRewards[user] = amount;
+        emit SetRewardsEvent(amount, user);
+    }
+
+    function isValid(string memory input) public pure returns (bool) {
+        bytes32 inputHash = keccak256(abi.encodePacked(input));
+        bytes32 headsHash = keccak256(abi.encodePacked("HEADS"));
+        bytes32 tailsHash = keccak256(abi.encodePacked("TAILS"));
+
+        return inputHash == headsHash || inputHash == tailsHash;
+    }
+}

e6649e90c6fe700fe05946e3ff4359fdc6460147
renzocabarios <renzo.cabarios@gmail.com>
2025-05-08 12:05:50 +0800

    Update

---
+++ contracts/script/deployment.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract DeploymentScript is Script {
+    Counter public counter;
+
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        counter = new Counter();
+
+        vm.stopBroadcast();
+    }
+}
--- contracts/src/KalboToken.sol
+++ contracts/src/KalboToken.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract KalboToken is ERC20 {
-    
-    constructor () ERC20 ("Kalbo Token", "KALBO"){
+    constructor() ERC20("Kalbo Token", "KALBO") {
         _mint(msg.sender, 1_000_000_000_000 * 10 ** decimals());
     }
-
 }

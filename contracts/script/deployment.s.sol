7e77f83ec89295a843f96519e2a8e8c055c7e43f
bordinano <vordigallardo@gmail.com>
2025-05-08 12:40:02 +0800

    Update deploy script

--- contracts/script/deployment.s.sol
+++ contracts/script/deployment.s.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Registry} from "../src/Registry.sol";
 import {KalboToken} from "../src/KalboToken.sol";
+import {Kalbonatics} from "../src/Kalbonatics.sol";
+import {Swap} from "../src/Swap.sol";
+import {Treasury} from "../src/Treasury.sol";
 
 contract DeploymentScript is Script {
     function setUp() public {}
@@ -18,6 +21,15 @@ contract DeploymentScript is Script {
 
         KalboToken kalboToken = new KalboToken();
         console.log("KalboToken Address:", address(kalboToken));
+
+        Kalbonatics kalbonatics = new Kalbonatics();
+        console.log("Kalbonatics Address:", address(kalbonatics));
+
+        Swap swap = new Swap();
+        console.log("Swap Address:", address(swap));
+
+        Treasury treasury = new Treasury();
+        console.log("Treasury Address:", address(treasury));
 
         vm.stopBroadcast();
     }
